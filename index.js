1c28638c52f6e5c7d4d9d6fc40292023ca5f4b9b
Suman <iamsuman898@gmail.com>
2022-08-14 16:35:03 +0530

    deploy fix hopeful

--- index.js
+++ index.js
@@ -6,33 +6,38 @@ const cors = require('cors');
 require('dotenv').config();
 // import our post router
 const postRoutes = require('./routes/posts.js');
-const BASE_URL =  process.env.DB_CONNECTION_URL;
 const path = require('path');
 
 // init express app
-const app = express(); 
+const app = express();
 
 
 // gen setup so that it can send requests properly
 // middleware
-app.use(bodyParser.json({limit:'30mb',extended:true})); // images that are going to be send have limit 30mb
-app.use(bodyParser.urlencoded({limit:'30mb',extended:true})); 
+app.use(bodyParser.json({ limit: '30mb', extended: true })); // images that are going to be send have limit 30mb
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
+
+
 // route to the posts
 app.use('/posts', postRoutes);
-app.get('/', (req,res)=> res.send("Welcome to the api"));
+app.get('/', (req, res) => res.send("Welcome to the api"));
 
+console.log(BASE_URL);//
 
 // other middlewares
-app.use(express.static(path.join(__dirname, "client" , "build")));
-app.get("*", (req,res)=>{
-    res.sendFile(path.resolve(__dirname, "client","build","index.html"));
-})
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "client", "build")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    })
+}
+
 
 
 const PORT = process.env.PORT || 5000;
 
 // function to connect with db : 3params : url, obj (2options), returns a promise
-mongoose.connect(BASE_URL, { useNewUrlParser:true , useUnifiedTopology :true})
-    .then(()=> app.listen(PORT,()=> console.log(`Db connected Server succesfully running on port ${PORT}`)))// when conn is established
-    .catch((error)=> console.log(`Error is ${error.message}`)) // error
+mongoose.connect(process.env.DB_CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Db connected Server succesfully running on port ${PORT}`)))// when conn is established
+    .catch((error) => console.log(`Error is ${error.message}`)) // error
